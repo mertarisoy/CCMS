a7dc93989d13b71b3302c4ed47984d52faef040a
Mert Arısoy <mertarisoy@gmail.com>
2015-05-20 11:36:14 +0300

    Entity Framework Added.

--- CreditCardManagementSystem/Controllers/HomeController.cs
+++ CreditCardManagementSystem/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CreditCardManagementSystem.Models;
 
 namespace CreditCardManagementSystem.Controllers
 {
@@ -10,6 +11,7 @@ namespace CreditCardManagementSystem.Controllers
     {
         public ActionResult Index()
         {
+            CardInfo cardInfo = new CardInfo();
             return View();
         }
         [Authorize(Roles="admin")]
